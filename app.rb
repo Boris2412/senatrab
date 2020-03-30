f5995cf88f8d862f4703263207a946b3c708e87a
Boris Bolshakov <boris.bolsh@gmail.com>
2020-03-30 21:40:49 +0300

    Fix form visit page

--- app.rb
+++ app.rb
@@ -14,3 +14,20 @@ get '/visit' do
   erb :visit
 end
 
+post '/visit' do
+  # username, phone, datetime
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @baber = params[:barber]
+
+  @title = "Thank you!"
+  @message = "Уважаемый #{@username}, мы ждём вас #{@datetime} у выбранного парикмахера #{@barber}."
+
+  # запишем в файл то, что ввёл клиент
+  f = File.open 'users.txt', 'a'
+  f.write "User: #{@username}, phone: #{@phone}, date and time: #{@datetime}. Baber: #{@barber}.\n"
+  f.close
+
+  erb :message
+end
